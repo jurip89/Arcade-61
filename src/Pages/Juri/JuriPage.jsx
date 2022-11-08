e3722233293873b59beaba4e5ca28a990b35db84
jurip89 <juriparladore@MacBook-Pro-79.local>
2022-11-08 20:39:14 +0100

    created base for game

--- src/Pages/Juri/JuriPage.jsx
+++ src/Pages/Juri/JuriPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./style.css";
 
 const JuriPage = () => {
-  return (
-    <div>JuriPage</div>
-  )
-}
+  const squares = new Array(256).fill({ name: "space" });
+  console.log(squares);
+  const alienInvaders = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39,
+  ];
 
-export default JuriPage
+  return (
+    <div className="container">
+      <div className="grid">
+        {squares.map((el, i) => (
+          <div
+            key={i}
+            className={
+              alienInvaders.includes(i) ? `${el.name} invader` : el.name
+            }
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default JuriPage;
